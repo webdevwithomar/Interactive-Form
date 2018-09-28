c12b215999ffdb04cab1856d85e029d173377f96
Omar Faruque <omar.faruque967@gmail.com>
2018-09-29 04:26:38 +0600

    Done with many things

--- js/script.js
+++ js/script.js
@@ -4,17 +4,32 @@ Basic Info
 
 $('#name').focus(); // By default, the name input is focused
 
-const $otherJob = $('<input type="text" id="other-title" placeholder="Your Job Role">') // Creates a text field, gives it an id attribute and a placeholder attribute and stores it in a variable
-
-$('fieldset:first-child').append($otherJob); // Selects the first fieldset and appends the other-job text field
+const $otherJob = $('#other-title'); // Selects the other job text field
 
 $($otherJob).hide(); // By Default the text field is hidden
 
-$('#title').on('change', function () { // Listens for any select option change
-  if ($(this).val() === 'other') { // Checks if the option has the value 'other'
-    $($otherJob).show(); // Shows the text field
+/*
+- The event listener below, fires whenever
+the status of the select field with the
+id 'title' changes.
+
+- The conditional statement inside the event
+handler checks if the option inside the select
+element is selected and has the value of
+other.
+
+- If the condition is true, the other job text
+field is shown.
+
+- If the condition becomes false, the text field
+becomes hidden again.
+*/
+
+$('#title').on('change', function () {
+  if ($(this).val() === 'other') { 'other'
+    $($otherJob).show();
   } else {
-    $($otherJob).hide(); // Hides the text field
+    $($otherJob).hide();
   }
 });
 
@@ -60,8 +75,12 @@ Register for Activities
 
 const checkboxes = $('input:checkbox'); // Selects the checkbox and stores it in a variable
 
+const payment = $('<h2></h2>'); // Creates a new h2 element
+
+$('.activities').append(payment); // Append it to the DOM
+
 /*
-- The Event Listener below checks to see if the status
+- The Event Listener below fires if the status
 of a checkbox is changed.
 
 - If a checkbox is changed, the conditional statement
@@ -74,75 +93,101 @@ are disabled by giving an attribute to the checkbox
 - The text color beside diabled event checkboxes are
 also changed to grey by selecting the checkbox's parent
 element and setting it's text color to #bbb.
+
+- The amount of money adds to the amountOfMoney variable
 */
+
+let amountOfMoney = 0; // Variable to track the number of money
 
 checkboxes.on('change', function () {
   const isChecked = $(this).is(':checked');
   if (isChecked) {
+    if ($(this).attr('name') === 'all') {
+      amountOfMoney += 200;
+    }
     if ($(this).attr('name') === 'js-frameworks') {
       $('input[name="express"]').attr('disabled', 'disabled');
       $('input[name="express"]').parent().css('color', '#bbb');
+      amountOfMoney += 100;
     }
     if ($(this).attr('name') === 'express') {
       $('input[name="js-frameworks"]').attr('disabled', 'disabled');
       $('input[name="js-frameworks"]').parent().css('color', '#bbb');
+      amountOfMoney += 100;
     }
     if ($(this).attr('name') === 'js-libs') {
       $('input[name="node"]').attr('disabled', 'disabled');
       $('input[name="node"]').parent().css('color', '#bbb');
+      amountOfMoney += 100;
     }
     if ($(this).attr('name') === 'node') {
       $('input[name="js-libs"]').attr('disabled', 'disabled');
       $('input[name="js-libs"]').parent().css('color', '#bbb');
+      amountOfMoney += 100;
     }
     if ($(this).attr('name') === 'build-tools') {
       $('input[name="npm"]').attr('disabled', 'disabled');
       $('input[name="npm"]').parent().css('color', '#bbb');
+      amountOfMoney += 100;
     }
     if ($(this).attr('name') === 'npm') {
       $('input[name="build-tools"]').attr('disabled', 'disabled');
       $('input[name="build-tools"]').parent().css('color', '#bbb');
+      amountOfMoney += 100;
     }
   }
 
   /*
-  - The conditional statement below, checks to see which 
+  - The conditional statement below, checks to see if a 
   checkbox has not been checked.
 
   - If the condition is true, the competing events
-  are enabled by removing the attribute of a checkbox 
+  are enabled by removing the attribute of their checkbox 
   'disabled'.
 
   - The text color beside enabled event checkboxes are
   also changed to their original color by selecting the 
   checkbox's parent element and removing its style
   attribute.
+
+  - Substracts the amount of money if unchecks the checkbox.
   */
 
   if (!isChecked) {
+    if ($(this).attr('name') === 'all') {
+      amountOfMoney -= 200;
+    }
     if ($(this).attr('name') === 'js-frameworks') {
       $('input[name="express"]').removeAttr('disabled', 'disabled');
       $('input[name="express"]').parent().removeAttr('style');
+      amountOfMoney -= 100;
     }
     if ($(this).attr('name') === 'express') {
       $('input[name="js-frameworks"]').removeAttr('disabled', 'disabled');
       $('input[name="js-frameworks"]').parent().removeAttr('style');
+      amountOfMoney -= 100;
     }
     if ($(this).attr('name') === 'js-libs') {
       $('input[name="node"]').removeAttr('disabled', 'disabled');
       $('input[name="node"]').parent().removeAttr('style');
+      amountOfMoney -= 100;
     }
     if ($(this).attr('name') === 'node') {
       $('input[name="js-libs"]').removeAttr('disabled', 'disabled');
       $('input[name="js-libs"]').parent().removeAttr('style');
+      amountOfMoney -= 100;
     }
     if ($(this).attr('name') === 'build-tools') {
       $('input[name="npm"]').removeAttr('disabled', 'disabled');
       $('input[name="npm"]').parent().removeAttr('style');
+      amountOfMoney -= 100;
     }
     if ($(this).attr('name') === 'npm') {
       $('input[name="build-tools"]').removeAttr('disabled', 'disabled');
       $('input[name="build-tools"]').parent().removeAttr('style');
+      amountOfMoney -= 100;
     }
   }
+
+  $('.activities h2').text(`Total: $${amountOfMoney}`); // Set the text content of the h2
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Interactive Form
+### A user friendly interactive form
+
+1. The form uses unobtrusive javascript.
+2. The form has been made with progressive enhancement in mind.
