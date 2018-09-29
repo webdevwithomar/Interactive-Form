8ce1266ba2702c752730ff8f9025cc74164a2d43
Omar Faruque <omar.faruque967@gmail.com>
2018-09-29 20:18:50 +0600

    Almost done everything

--- js/script.js
+++ js/script.js
@@ -39,6 +39,15 @@ T-Shirt Info
 
 $('#colors-js-puns').hide(); // By default, the div containing t-shirt color option is hidden
 
+// The function below takes some option elements as arguments, hides or shows them and adds or removes attributes from them
+function showHideColors (hide, show, addAttr, rmvAttr) {
+  $('#colors-js-puns').show();
+  $(hide).hide();
+  $(show).show();
+  $(addAttr).attr('selected', 'selected');
+  $(rmvAttr).removeAttr('selected');
+}
+
 /*
 - The event listener below fires whenever the
 status of a select element is changed.
@@ -52,18 +61,20 @@ shows and hides the color options.
 
 $('#design').on('change', function(){
   if ($('#design option:selected').attr('value') === 'heart js') {
-    $('#colors-js-puns').show();
-    $('#color option:nth-child(-1n+3)').hide();
-    $('#color option:nth-child(1n+4)').show();
-    $('#color option:nth-child(4)').attr('selected', 'selected');
-    $('#color option:nth-child(1)').removeAttr('selected');
+    showHideColors (
+    '#color option:nth-child(-1n+3)',
+    '#color option:nth-child(1n+4)',
+    '#color option:nth-child(4)',
+    '#color option:nth-child(1)'
+    )
   }
   else if ($('#design option:selected').attr('value') === 'js puns') { 
-    $('#colors-js-puns').show();
-    $('#color option:nth-child(1n+4)').hide();
-    $('#color option:nth-child(-1n+3)').show();
-    $('#color option:nth-child(1)').attr('selected', 'selected');
-    $('#color option:nth-child(4)').removeAttr('selected');
+    showHideColors (
+    '#color option:nth-child(1n+4)',
+    '#color option:nth-child(-1n+3)',
+    '#color option:nth-child(1)',
+    '#color option:nth-child(4)'
+    )
   } else {
     $('#colors-js-puns').hide();
   }
@@ -78,6 +89,15 @@ const checkboxes = $('input:checkbox'); // Selects the checkbox and stores it in
 const payment = $('<h2></h2>'); // Creates a new h2 element
 
 $('.activities').append(payment); // Append it to the DOM
+
+let amountOfMoney = 0; // Variable to track the number of money
+
+// This function takes an input as an argument and gives the input an attribute, selects its parent and sets the text color.
+function activitiesSection (selectInputwithNameAttr) {
+  $(selectInputwithNameAttr).attr('disabled', 'disabled');
+  $(selectInputwithNameAttr).parent().css('color', '#bbb');
+  amountOfMoney += 100;
+}
 
 /*
 - The Event Listener below fires if the status
@@ -97,8 +117,6 @@ element and setting it's text color to #bbb.
 - The amount of money adds to the amountOfMoney variable
 */
 
-let amountOfMoney = 0; // Variable to track the number of money
-
 checkboxes.on('change', function () {
   const isChecked = $(this).is(':checked');
   if (isChecked) {
@@ -106,36 +124,31 @@ checkboxes.on('change', function () {
       amountOfMoney += 200;
     }
     if ($(this).attr('name') === 'js-frameworks') {
-      $('input[name="express"]').attr('disabled', 'disabled');
-      $('input[name="express"]').parent().css('color', '#bbb');
-      amountOfMoney += 100;
+      activitiesSection('input[name="express"]');
     }
     if ($(this).attr('name') === 'express') {
-      $('input[name="js-frameworks"]').attr('disabled', 'disabled');
-      $('input[name="js-frameworks"]').parent().css('color', '#bbb');
-      amountOfMoney += 100;
+      activitiesSection('input[name="js-frameworks"]');
     }
     if ($(this).attr('name') === 'js-libs') {
-      $('input[name="node"]').attr('disabled', 'disabled');
-      $('input[name="node"]').parent().css('color', '#bbb');
-      amountOfMoney += 100;
+      activitiesSection('input[name="node"]');
     }
     if ($(this).attr('name') === 'node') {
-      $('input[name="js-libs"]').attr('disabled', 'disabled');
-      $('input[name="js-libs"]').parent().css('color', '#bbb');
-      amountOfMoney += 100;
+      activitiesSection('input[name="js-libs"]');
     }
     if ($(this).attr('name') === 'build-tools') {
-      $('input[name="npm"]').attr('disabled', 'disabled');
-      $('input[name="npm"]').parent().css('color', '#bbb');
-      amountOfMoney += 100;
+      activitiesSection('input[name="npm"]');
     }
     if ($(this).attr('name') === 'npm') {
-      $('input[name="build-tools"]').attr('disabled', 'disabled');
-      $('input[name="build-tools"]').parent().css('color', '#bbb');
-      amountOfMoney += 100;
+      activitiesSection('input[name="build-tools"]');
     }
   }
+
+  // This function takes an input as an argument and removes an attribute, selects its parent and removes the style attribute.
+ function activitiesSection2 (selectInputwithNameAttr) {
+  $(selectInputwithNameAttr).removeAttr('disabled', 'disabled');
+  $(selectInputwithNameAttr).parent().removeAttr('style');
+  amountOfMoney -= 100;
+}
 
   /*
   - The conditional statement below, checks to see if a 
@@ -158,36 +171,101 @@ checkboxes.on('change', function () {
       amountOfMoney -= 200;
     }
     if ($(this).attr('name') === 'js-frameworks') {
-      $('input[name="express"]').removeAttr('disabled', 'disabled');
-      $('input[name="express"]').parent().removeAttr('style');
-      amountOfMoney -= 100;
+      activitiesSection2 ('input[name="express"]')
     }
     if ($(this).attr('name') === 'express') {
-      $('input[name="js-frameworks"]').removeAttr('disabled', 'disabled');
-      $('input[name="js-frameworks"]').parent().removeAttr('style');
-      amountOfMoney -= 100;
+      activitiesSection2 ('input[name="js-frameworks"]')
     }
     if ($(this).attr('name') === 'js-libs') {
-      $('input[name="node"]').removeAttr('disabled', 'disabled');
-      $('input[name="node"]').parent().removeAttr('style');
-      amountOfMoney -= 100;
+      activitiesSection2 ('input[name="node"]')
     }
     if ($(this).attr('name') === 'node') {
-      $('input[name="js-libs"]').removeAttr('disabled', 'disabled');
-      $('input[name="js-libs"]').parent().removeAttr('style');
-      amountOfMoney -= 100;
+      activitiesSection2 ('input[name="js-libs"]')
     }
     if ($(this).attr('name') === 'build-tools') {
-      $('input[name="npm"]').removeAttr('disabled', 'disabled');
-      $('input[name="npm"]').parent().removeAttr('style');
-      amountOfMoney -= 100;
+      activitiesSection2 ('input[name="npm"]')
     }
     if ($(this).attr('name') === 'npm') {
-      $('input[name="build-tools"]').removeAttr('disabled', 'disabled');
-      $('input[name="build-tools"]').parent().removeAttr('style');
-      amountOfMoney -= 100;
+      activitiesSection2 ('input[name="build-tools"]')
     }
   }
 
   $('.activities h2').text(`Total: $${amountOfMoney}`); // Set the text content of the h2
+});
+
+/*****************************
+Payment Info
+******************************/
+
+$('#payment option[value="credit card"]').attr('selected', true); // Credit card option is selected by default
+
+$('#payment option[value="select_method"]').hide(); // Select from method is hide
+
+// Selecting the div with the ID credit-card
+const creditCard = $('#credit-card');
+
+// Selecting the div that is the sibling of the div with the ID credit-card and setting its id to paypal
+$('#credit-card').next().attr('id', 'paypal');
+
+// Selecting the div with the id paypal and storing it to a variable
+const paypal = $('#paypal');
+
+// Selecting the div that is the sibling of the div with the ID credit-card and setting its id to bitcoin
+$('#credit-card').next().next().attr('id', 'bitcoin');
+
+// Selecting the div with the id bitcoin and storing it to a variable
+const bitCoin = $('#bitcoin');
+
+// A function that takes the payment method divs as arguments and show or hide them
+function paymentMethods (paymentShow, paymentHide, paymentHide1) {
+  $(paymentShow).show();
+  $(paymentHide).hide();
+  $(paymentHide1).hide();
+}
+
+/*
+- The event listener below fires whenever 
+changes happen to the payment select option
+
+- If the credit card option is selected, the
+credit card boxese are shown and other two
+payment boxes are hidden
+
+- If the paypal option is selected, the
+paypal box is shown and other two
+payment boxes are hidden
+
+- If the bitcoin option is selected, the
+bitcoin box is shown and other two
+payment boxes are hidden
+*/
+
+$('#payment').on('change', function () {
+  if ($('#payment option:selected').attr('value') === 'credit card') {
+    paymentMethods(creditCard, paypal, bitCoin);
+  }
+  else if ($('#payment option:selected').attr('value') === 'paypal') {
+    paymentMethods(paypal, creditCard, bitCoin);
+  }
+  else if ($('#payment option:selected').attr('value') === 'bitcoin') {
+    paymentMethods(bitCoin, creditCard, paypal);
+  }
+});
+
+/*****************************
+Form Validation
+******************************/
+// Below is a function for errorMessages
+function errorMessage(element, text) {
+  const message = $('<p>'+text+'</p>');
+  $('form').append(message);
+  $(message).css('color', 'firebrick');
+  $(message).insertBefore(element);
+}
+
+$('form').on('submit',function(e){
+  if ($('#name').val() === '') {
+    e.preventDefault();
+    errorMessage($('#name'), 'Name field cannot be blank')
+  }
 });
